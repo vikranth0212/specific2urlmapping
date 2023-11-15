a145a062453ed8fa0e8ecd134edf7f3314be336d
vikranth0212 <vikranth15.6.1995@gmail.com>
2023-11-15 18:56:25 +0530

    first commit

---
+++ nz/urls.py
@@ -0,0 +1,6 @@
+from nz.views import *
+from django.urls import path
+app_name='philleps'
+urlpatterns=[
+    path('will/',will,name='will'),
+]
---
+++ nz/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def will(request):
+    return HttpResponse('hi i am will')
